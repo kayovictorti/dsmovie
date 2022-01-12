6e833e89fea6036402713a716b5f320dccc58931
Kayo Victor <kayoamorim@gmail.com>
2022-01-12 20:52:58 -0300

    Routes

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar() {
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/kayovictorti">
+                    <a href="https://github.com/kayovictorti" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/kayovictorti</p>
